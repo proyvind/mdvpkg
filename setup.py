03182aaa2bec1119e8c89a88926c93b8f6fe9299
J. Victor Martins <jvdm@mandriva.com>
2011-03-04 11:58:34 -0300

    Cleaning setup script.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+import mdvpkgd
 from distutils.core import setup
 from glob import glob
 
-data_files = [('/etc/dbus-1/system.d/', glob('dbus/*.conf')),
-              ('/usr/share/dbus-1/system-services/', glob('dbus/*.service'))]
+data_files = [ ('/etc/dbus-1/system.d/',
+                glob('dbus/*.conf')),
+               ('/usr/share/dbus-1/system-services/',
+                glob('dbus/*.service')) ]
 
 setup(name='mdvpkgd',
-      version='0.1.0',
+      version=mdvpkgd.__version__,
       description='Mandriva Package Daemon',
+      license='GNU GPLv3',
       author='J. Victor Martins',
       author_email='jvdm@mandriva.com',
       packages=['mdvpkgd'],
-      data_files=data_files
-     )
+      scripts=['bin/mdvpkgd'],
+      data_files=data_files)
